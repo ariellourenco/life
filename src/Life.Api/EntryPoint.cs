3122d74a0384d93d4528707f670562a2e1abddbe
Ariel Lourenco <ariellourenco@users.noreply.github.com>
2025-08-08 19:31:15 -0300

    Update solution file and project packages

--- src/Life.Api/EntryPoint.cs
+++ src/Life.Api/EntryPoint.cs
@@ -1,5 +1,5 @@
-/// <summary>
-/// Unfortunatly, we need to make a partial method to expose the entry point for
-// our intergration tests. This limitation has been fixed in .NET 9.
-/// </summary>
+
+// Unfortunatly, we need to make a partial method to expose the entry point for our integration tests.
+// This limitation has been fixed in .NET 10 by adding a source generator that emits this code.
+// For further details: https://github.com/dotnet/csharplang/issues/6769
 public partial class Program { }
